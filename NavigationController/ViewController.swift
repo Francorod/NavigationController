fddc1f0f9e50ec7e180ccadaedd4b20f5eb3f7db
Franco Rod <81878824+Francorod@users.noreply.github.com>
2022-03-14 19:43:49 -0400

    Relationship between view controller

--- NavigationController/ViewController.swift
+++ NavigationController/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func showFirstViewController(_ sender: Any) {
+        performSegue(withIdentifier: "SegueToFirstVC", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Root View"
     }
-
-
+    
+    
 }
 
